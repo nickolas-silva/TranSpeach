cb912930a97c6fedc3cce7f0f153c72a9dd5e826
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2024-08-29 14:27:33 -0300

    adding dotenv and setup of openAi package

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:transpeach/app/routes/app_pages.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:dart_openai/dart_openai.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  OpenAI.apiKey = dotenv.env["OPEN_AI_API_KEY"]!;
   runApp(const TranSpeachApp());
 }
-
 
 class TranSpeachApp extends StatelessWidget {
   const TranSpeachApp({super.key});
@@ -17,8 +20,9 @@ class TranSpeachApp extends StatelessWidget {
         fontFamily: 'Poppins',
         primarySwatch: Colors.purple,
       ),
-      builder: (context, child) => 
-        MediaQuery(data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true), child: child!),
+      builder: (context, child) => MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child!),
       navigatorKey: Get.key,
       debugShowCheckedModeBanner: false,
       initialRoute: Routes.home,
