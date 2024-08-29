53f3fd3c071c1985954c4c082eb6e344f8a6a468
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2024-08-29 14:31:12 -0300

    adding audioplayers package

--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -1,5 +1,24 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
+import 'package:dart_openai/dart_openai.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeController extends GetxController {
   String teste = 'teste';
+}
+
+void textToSpeech(String text) async {
+// The speech request.
+  File speechFile = await OpenAI.instance.audio.createSpeech(
+    model: "tts-1",
+    input: "Say my name is Anas",
+    voice: "nova",
+    responseFormat: OpenAIAudioSpeechResponseFormat.mp3,
+    outputDirectory: await Directory("speechOutput").create(),
+    outputFileName: "anas",
+  );
+
+// The file result.
+  print(speechFile.path);
 }
